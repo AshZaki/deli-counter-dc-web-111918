fe55bab7dac3e9125fba39a70d20c8923beab2a1
Ashley  Zaki <ahnahn.un@gmail.com>
2018-10-22 03:28:41 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,8 +5,11 @@ def line(current_line)
     curr = "The line is currently:"
     current_line.each_with_index do |line,index|
     curr += " #{index + 1}. #{line}"
-    
     end
     puts "#{curr}"
   end
+end
+
+def take_a_number(line,name)
+  
 end
