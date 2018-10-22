03b4f118d29a13f8cf6ee318205b793aa5b5d146
Ashley  Zaki <ahnahn.un@gmail.com>
2018-10-22 03:14:25 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,6 +3,6 @@ def line(current_line)
   if current_line = 0
     puts "The line is currently empty." 
   elsif current_line > 0
-    puts "The line is currently:#{current}"
+    puts "The line is currently:#{current_line}"
   end
 end
