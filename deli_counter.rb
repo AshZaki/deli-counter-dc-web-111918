f3751f48a7c4588baf6a61b5004678e1dcd52f10
Ashley  Zaki <ahnahn.un@gmail.com>
2018-10-22 03:13:57 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,8 +3,7 @@ def line(current_line)
   if current_line = 0
     puts "The line is currently empty." 
   elsif current_line > 0
-    current_line += 1
-    katz_dali.push("The line is currently:")
+    puts "The line is currently:#{current}"
   end
   return katz_dali
 end
