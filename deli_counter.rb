06e62d8e40ec43cbb0146a5b0f24ccaf7bdc429b
Ashley  Zaki <ahnahn.un@gmail.com>
2018-10-22 03:21:38 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(current_line)
   elsif
     curr = "The line is currently: "
     
-    current_line.each_with_index do 
+    current_line.each_with_index do |line,index|
     return katz_dali
   end
 end
